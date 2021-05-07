fc11960b63351bc33241ffe51d072ecf2526780e
HicaroD <hdanrlley1@gmail.com>
2021-05-07 14:40:42 -0300

    Adding more features(Description)
    Adding more features about clutches, assistances, top 3 weapons and deaths

--- main.py
+++ main.py
@@ -8,43 +8,75 @@ class Valorant(object):
     def __init__(self, nick_name, riot_id):
         self.nick_name = nick_name
         self.riot_id = riot_id
-
-
-    def get_rank(self):
+        
+    def web_page_stats(self):
         source = requests.get(url = f'https://tracker.gg/valorant/profile/riot/{self.nick_name}%23{self.riot_id}/overview'.replace(' ', '+')).text
         soup = bs(source, 'lxml')
+        return soup
+    
+    def match_history_page(self):
+        source = requests.get(url= f'https://tracker.gg/valorant/profile/riot/{self.nick_name}%23{self.riot_id}/matches'.replace(' ', '+')).text
+        soup = bs(source, 'lxml')
+        return soup 
+    
+    def get_rank(self):
+        my_page = self.web_page_stats()
 
-        rank_information = soup.find('div', class_="valorant-highlighted-stat").find('span', class_="valorant-highlighted-stat__value").text
-        return f'Your rank is {rank_information}'
+        rank_information = my_page.find('div', class_="valorant-highlighted-stat").find('span', class_="valorant-highlighted-stat__value").text
+        return f'The rank of {self.nick_name} is {rank_information}'
 
     def victories(self):
-        source = requests.get(url=f'https://tracker.gg/valorant/profile/riot/{self.nick_name}%23{self.riot_id}/overview'.replace(' ', '+')).text
-        soup = bs(source, 'lxml')
+        victories_page = self.web_page_stats()
         
-        main_section_victories_counter = soup.findAll('div', class_="stat align-left expandable")[0].text.split('  ')
-        return f'You have won {main_section_victories_counter[2]} match(es)'
+        main_section_victories_counter = victories_page.findAll('div', class_="stat align-left expandable")[0].text.split('  ')
+        return f'{self.nick_name} has won {main_section_victories_counter[2]} match(es)'
     
-        # for i in range(10):
-        #     main_section_victories_counter = soup.findAll('div', class_="stat align-left expandable")[i].text.split('  ')
-        #     print(main_section_victories_counter)
-    def kills_counter(self):
-        source = requests.get(url=f'https://tracker.gg/valorant/profile/riot/{self.nick_name}%23{self.riot_id}/overview'.replace(' ', '+')).text
-        soup = bs(source, 'lxml')
-        number_kills = soup.findAll('div', class_="stat align-left expandable")[1].text.split('  ')
+    def kills(self):
+        kills_page = self.web_page_stats()
+        number_kills = kills_page.findAll('div', class_="stat align-left expandable")[1].text.split('  ')
         
-        return f'You have {number_kills[2]} kill(s)'
+        return f'{self.nick_name} has {number_kills[2]} kill(s)'
     
-    def headshot_counter(self):
-        source = requests.get(url=f'https://tracker.gg/valorant/profile/riot/{self.nick_name}%23{self.riot_id}/overview'.replace(' ', '+')).text
-        soup = bs(source, 'lxml')
-        number_kills = soup.findAll('div', class_="stat align-left expandable")[2].text.split('  ')
+    def headshot(self):
+        headshot_page = self.web_page_stats()
+        number_kills = headshot_page.findAll('div', class_="stat align-left expandable")[2].text.split('  ')
         
-        return f'You have {number_kills[2]} headshot(s)'
+        return f'{self.nick_name} has {number_kills[2]} headshot(s)'      
+    
+    def deaths(self):
+        deaths_page = self.web_page_stats()
+        number_deaths = deaths_page.findAll('div', class_="stat align-left expandable")[3].text.split('  ')
         
-
+        return f'{self.nick_name} has {number_deaths[2]} death(s)'
+    def assistances(self):
+        assistances_page = self.web_page_stats()
+        number_assistances = assistances_page.findAll('div', class_="stat align-left expandable")[4].text.split('  ')
+        
+        return f'{self.nick_name} has {number_assistances[2]} assistance(s)'
+    def clutches(self):
+        clutches_pages = self.web_page_stats()
+        number_clutches = clutches_pages.findAll('div', class_="stat align-left expandable")[7].text.split('  ') 
+        
+        return f'{self.nick_name} has won {number_clutches[2]} clutch(es)'
+    
+    def top_weapons(self):
+        weapons_page = self.web_page_stats()
+        
+        print(f'\n{self.nick_name}, your top weapons:\n')
+        
+        for index in range(3):
+                weapon_name = weapons_page.findAll('div', class_="weapon__name")[index].text
+                top_weapons_kills = weapons_page.findAll('div', class_='weapon__main-stat')[index].text
+                
+                print(f'{weapon_name} ---> {top_weapons_kills}')
+    
 #Insert the nickname of the player and your Riot ID to get your stats, such as rank, kills, victories and headshot
-player_1 = Valorant('TXC BOLADONA', '2050')
-print(player_1.get_rank())
-print(player_1.victories())
-print(player_1.kills_counter())
-print(player_1.headshot_counter())
+player_1 = Valorant('TXC H1CARO', '6761')
+print(player_1.get_rank()) #Get your rank information
+print(player_1.victories()) # Do you want to see how many victories you have?
+print(player_1.kills()) # Wanna see how many kill you get?
+print(player_1.headshot()) # What about headshot? 
+print(player_1.deaths()) # How many times you died? 
+print(player_1.assistances()) # Do you help your team? Check it out 
+print(player_1.clutches()) # Are you really strong at this game? Check how many clutches you achieved
+player_1.top_weapons() # See your top weapons based on your kills
